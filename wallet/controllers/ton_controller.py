9c74a14228c8b999da6a3c951c45ba2a1735fd7f
JesterBoot <Aleksakhinga@yandex.ru>
2024-07-02 14:49:43 +0400

    fix/jetton_error: logging get_jetton_wallet

--- wallet/controllers/ton_controller.py
+++ wallet/controllers/ton_controller.py
@@ -25,7 +25,12 @@ class TonController:
         return address
 
     async def get_jetton_wallet(self, address: Address):
-        return await self.ton_client.tc_client.get_jetton_wallet(address.to_string())
+        try:
+            jetton_wallet = await self.ton_client.tc_client.get_jetton_wallet(address.to_string())
+            return jetton_wallet
+        except TonTools.Providers.TonCenterClient.GetMethodError as e:
+            logging.error(f"Error getting jetton wallet data for address {address=}: {e=}")
+            return None
 
     async def get_jetton_data(self, master_address: Address):
         return await self.ton_client.tc_client.get_jetton_data(
